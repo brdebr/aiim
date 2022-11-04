ec46f088ecee53ad459d83f855b3ed9969647d00
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2022-11-04 16:02:03 +0100

    Add prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['./.env.dev', './.env'],
+    }),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from '.prisma/client';
+import {
+  Injectable,
+  Logger,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  private readonly logger = new Logger(PrismaService.name);
+
+  constructor(config: ConfigService) {
+    const url = config.get<string>('DATABASE_URL');
+    super({
+      datasources: {
+        db: {
+          url,
+        },
+      },
+    });
+    this.logger.log(`Connecting to database ->`);
+    this.logger.log(`${url}`);
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log(`Connected to database 🚀`);
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    this.logger.log(`Disconnected from database 👋`);
+  }
+}
