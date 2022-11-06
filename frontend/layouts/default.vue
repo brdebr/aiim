c9d2ff4f3cb1a7d5295c550b352921e8bb70efaf
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2022-11-06 15:15:38 +0100

    Center app title

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -4,9 +4,11 @@
       <template v-if="showingDrawerButton" v-slot:prepend>
           <v-app-bar-nav-icon/>
       </template>
-      <v-app-bar-title>
+      <v-app-bar-title class="w--mx-4 w--text-center">
         AI-Image Manager
       </v-app-bar-title>
+      <div v-show="showSpacer" class="app-bar-spacer">
+      </div>
     </v-app-bar>
     <v-main>
       <div class="w--px-3">
@@ -22,18 +24,30 @@ import { storeToRefs } from 'pinia';
 const layoutStore = useLayoutStore();
 const { showingDrawerButton } = storeToRefs(layoutStore);
 
+const showSpacer = computed(() => {
+  return showingDrawerButton.value;
+});
+
 </script>
 <style lang="scss">
 html, body, .__nuxt, .v-application {
   height: 100vh;
   width: 100vw;
 }
-.mobile-layout, .v-main {
+.mobile-layout, .v-main, .v-main > div {
   height: 100%;
   width: 100%;
 }
 
 .v-main {
   background-color: rgb(243, 243, 230);
+}
+
+.v-app-bar {
+  .app-bar-spacer {
+    width: 48px;
+    height: 48px;
+    margin-right: 10px;
+  }
 }
 </style>
