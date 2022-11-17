14e333cc592fe90d3e768ae44733bd6fccced3f0
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2022-11-17 22:36:53 +0100

    Improve IoView

--- frontend/components/IoView.vue
+++ frontend/components/IoView.vue
@@ -1,20 +1,29 @@
 <template>
   <div class="io-view">
     <img
+      ref="imageEl"
       :data-width="props.image.width" :data-height="props.image.height"
       :data-id="props.image.id"
       :src="`${apiBaseURL}/api/images/view/${props.image.id}`"
       loading="lazy"
       :title="props.image.prompt" :alt="props.image.prompt"
+      @click="toggle"
     />
   </div>
 </template>
 <script lang="ts" setup>
 import { ImageObject } from '~~/types';
+const { getDimensions } = useImageUtils();
 const apiBaseURL = useApiBaseURL();
 const props = defineProps<{
   image: ImageObject;
 }>();
+
+const imageEl = ref<HTMLImageElement | null>(null);
+
+const { toggle } = useFullscreen(imageEl);
+
+const { isTall, isWide,  } = getDimensions(props.image);
 </script>
 <style lang="scss">
 .io-view {
