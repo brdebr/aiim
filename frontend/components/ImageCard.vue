0c663bc7e4eba5dae848e5ce1863f0930c71529b
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2022-11-28 23:51:40 +0100

    Add fullscreen to ImageCard manually

--- frontend/components/ImageCard.vue
+++ frontend/components/ImageCard.vue
@@ -1,20 +1,41 @@
 <template>
-  <v-card>
+  <v-card rounded="lg">
     <v-img
+      ref="imageEl"
       :alt="props.image.prompt"
       :title="props.image.prompt"
       :src="`${apiBaseURL}/api/images/view/${props.image.id}`"
       :aspect-ratio="props.image.width / props.image.height"
-      cover
+      @click="toggleFullscreen"
     >
       <slot />
     </v-img>
   </v-card>
 </template>
 <script setup lang="ts">
+import { VImg } from 'vuetify/components';
 import { ImageObject } from '~~/types';
 
 const apiBaseURL = useApiBaseURL();
+
+const isFullscreen = ref(false);
+const goFullscreen = () => {
+  imageEl.value?.image?.requestFullscreen();
+  isFullscreen.value = true;
+}
+const exitFullscreen = () => {
+  document.exitFullscreen();
+  isFullscreen.value = false;
+}
+const toggleFullscreen = () => {
+  if (isFullscreen.value) {
+    exitFullscreen();
+  } else {
+    goFullscreen();
+  }
+}
+
+const imageEl = ref<VImg>();
 
 const props = defineProps<{
   image: ImageObject;
