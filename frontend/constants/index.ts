c5c1d4a972d6c4b1d75e31db468491a6efa0d5d6
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2022-12-04 12:25:39 +0100

    Hardcode search into layout

--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -1,2 +1,33 @@
 export const apiBaseUrlDev = import.meta.env.DEV ? 'http://192.168.1.132:3006' : '';
 export const LOCAL_STORAGE_PREFIX = 'aiim-';
+
+export const Samplers = [
+  'Euler a',
+  'Euler',
+  'LMS',
+  'Heun',
+  'DPM2',
+  'DPM2 a',
+  'DPM++ 2S a',
+  'DPM++ 2M',
+  'DPM++ SDE',
+  'DPM fast',
+  'DPM adaptive',
+  'LMS Karras',
+  'DPM2 Karras',
+  'DPM2 a Karras',
+  'DPM++ 2S a Karras',
+  'DPM++ 2M Karras',
+  'DPM++ SDE Karras',
+  'DDIM',
+  'PLMS',
+] as const;
+
+export const modelHashesMap = {
+  '1.5-emaonly': '81761151',
+  '1.5-inpainting': '3e16efc8',
+  '1.4': '7460a6fa',
+  'bryanwd-person': 'da781e47',
+  '1.5-pruned': 'a9263745',
+  '2.0-768-v-ema': '2c02b20a',
+} as const;
--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="layout-container mobile-layout">
     <v-app-bar :elevation="0" :color="'indigo-darken-4'" border="t-md b-md s-lg e-lg" density="compact">
-      <v-app-bar-title class="qw-mx-4 qw-text-center">
-        <div class="qw-select-none" @click="$router.push('/')">
-          AI-Image Manager
+      <v-app-bar-title class="!qw-mx-0">
+        <div class="qw-mx-2 qw-flex qw-items-center">
+          <div class="qw-flex qw-gap-3 qw-items-center qw-select-none qw-mr-auto" @click="$router.push('/')">
+            <img src="/logo-min.png" class="qw-h-6 qw-w-6"/>
+            <span>
+              AI Image Manager
+            </span>
+          </div>
+          <div class="qw-hidden sm:qw-block qw-mr-6">
+            <v-btn variant="outlined" size="x-small" icon>
+              <v-icon>mdi-refresh</v-icon>
+            </v-btn>
+          </div>
+          <div class="qw-hidden sm:qw-block">
+            <v-btn variant="outlined" size="x-small" icon @click="searchDrawer = !searchDrawer">
+              <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+          </div>
         </div>
       </v-app-bar-title>
     </v-app-bar>
@@ -17,6 +32,109 @@
     <v-main>
       <slot/>
     </v-main>
+    <v-navigation-drawer
+      v-model="searchDrawer"
+      location="right"
+      color="indigo-darken-4"
+      :width="drawerWidth"
+      temporary
+    >
+      <div class="qw-flex qw-flex-col qw-gap-5 qw-px-3 qw-py-6">
+        <div class="qw-text-center">
+          Search
+        </div>
+        <v-text-field
+          v-model="search.prompt"
+          label="Prompt"
+          class="qw-mb-3"
+          variant="outlined"
+          density="compact"
+          hide-details
+          clearable
+        />
+        <v-text-field
+          v-model="search.negativePrompt"
+          label="Negative prompt"
+          class="qw-mb-3"
+          variant="outlined"
+          density="compact"
+          hide-details
+          clearable
+        />
+        <div class="qw-flex qw-gap-4">
+          <v-text-field
+            v-model.number="search.steps"
+            label="Steps"
+            class="qw-mb-3"
+            variant="outlined"
+            density="compact"
+            hide-details
+            clearable
+          />
+          <v-text-field
+            v-model.number="search.cfg"
+            label="CFG"
+            class="qw-mb-3"
+            variant="outlined"
+            density="compact"
+            hide-details
+            clearable
+          />
+        </div>
+        <div class="qw-flex qw-gap-4">
+          <v-text-field
+            v-model.number="search.width"
+            label="Width"
+            class="qw-mb-3"
+            variant="outlined"
+            density="compact"
+            hide-details
+            clearable
+          />
+          <v-text-field
+            v-model.number="search.height"
+            label="Height"
+            class="qw-mb-3"
+            variant="outlined"
+            density="compact"
+            hide-details
+            clearable
+          />
+        </div>
+        <ClientOnly>
+          <v-select
+            v-model="search.sampler"
+            label="Sampler"
+            class="qw-mb-3"
+            variant="outlined"
+            density="compact"
+            hide-details
+            :items="Samplers"
+            clearable
+          />
+          <v-select
+            v-model="search.model"
+            label="Model"
+            class="qw-mb-3"
+            variant="outlined"
+            density="compact"
+            hide-details
+            :items="modelsAsPairs"
+            item-title="0"
+            item-value="1"
+            clearable
+          />
+        </ClientOnly>
+        <div class="qw-flex qw-gap-3 qw-items-center">
+          <v-btn variant="outlined" class="qw-flex-grow">
+            Filter
+          </v-btn>
+          <v-btn @click="clearSearch" icon variant="outlined" size="x-small" class="!qw-rounded-sm">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </div>
+      </div>
+    </v-navigation-drawer>
     <v-bottom-navigation
       v-if="userId"
       class="bottom-bar"
@@ -39,12 +157,63 @@
   </div>
 </template>
 <script setup lang="ts">
+import { Samplers, modelHashesMap } from '~~/constants';
+
+const breakpoints = useBreakpoints({
+  mobile: 915,
+  wide: 1080,
+})
+
+const drawerWidth = computed(() => {
+  if(breakpoints.isSmallerOrEqual('mobile')) return 325;
+  if(breakpoints.isSmallerOrEqual('wide')) return 400;
+  return 500;
+})
+
+const modelsAsPairs = Object.entries(modelHashesMap);
+
+type ImageSearchType = {
+  prompt: string;
+  negativePrompt: string;
+  steps: number;
+  cfg: number;
+  width: number;
+  height: number;
+  sampler: typeof Samplers[number];
+  model: typeof modelsAsPairs[number][1];
+}
+
+const search = reactive<Partial<ImageSearchType>>({});
+
+const clearSearch = () => {
+  search.prompt = undefined;
+  search.negativePrompt = undefined;
+  search.steps = undefined;
+  search.cfg = undefined;
+  search.width = undefined;
+  search.height = undefined;
+  search.sampler = undefined;
+  search.model = undefined; 
+}
+
+// const search = reactive<ImageSearchType>({
+//   prompt: '',
+//   negativePrompt: '',
+//   steps: 28,
+//   cfg: 7.5,
+//   width: 768,
+//   height: 768,
+//   sampler: 'Euler a',
+//   model: '2c02b20a',
+// });
 
 const layoutStore = useLayoutStore();
 const { bottomNavigationItems } = storeToRefs(layoutStore);
 
 const authStore = useAuthStore();
 const { userId } = storeToRefs(authStore);
+
+const searchDrawer = ref(false);
 
 useHead({
   meta: [
