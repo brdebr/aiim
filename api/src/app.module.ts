e0abcb0a3a0cf21b45784ef1cf2686ea1492182c
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2022-12-05 22:59:46 +0100

    Rename BullModule file

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -7,7 +7,7 @@ import { AuthModule } from './auth/auth.module';
 import { GlobalJwtGuardProvider } from './auth/jwt.guard';
 import { ConfigurationModule } from './config';
 import { ImageGenerationModule } from './image-generation/image-generation.module';
-import { BullQueuesModule } from './redis';
+import { BullQueuesModule } from './queues';
 import { EventEmmiter } from './events';
 
 @Module({
