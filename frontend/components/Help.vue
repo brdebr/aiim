6df1201471ec492f5dadb9567828922c525e84d9
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2022-12-06 17:46:42 +0100

    Add Help component

---
+++ frontend/components/Help.vue
@@ -0,0 +1,14 @@
+<template>
+  <ClientOnly>
+    <v-tooltip location="top" content-class="bg-white text-indigo-darken-4">
+      <template #activator="{ props }">
+        <v-icon size="small" class="qw-ml-1" v-bind="props">
+          mdi-help-circle
+        </v-icon>
+      </template>
+      <span class="qw-whitespace-pre-line">
+        <slot />
+      </span>
+    </v-tooltip>
+  </ClientOnly>
+</template>
