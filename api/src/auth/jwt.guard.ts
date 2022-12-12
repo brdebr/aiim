c930725b370e255365482cebddde7841bf36413b
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2022-12-12 18:31:17 +0100

    Load images count at application startup

--- api/src/auth/jwt.guard.ts
+++ api/src/auth/jwt.guard.ts
@@ -1,4 +1,4 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, Provider } from '@nestjs/common';
 import { APP_GUARD, Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 
@@ -20,7 +20,7 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
   }
 }
 
-export const GlobalJwtGuardProvider = {
+export const GlobalJwtGuardProvider: Provider = {
   provide: APP_GUARD,
   useClass: JwtAuthGuard,
 };
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,6 +1,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,9 +14,15 @@ async function bootstrap() {
   const port = process.env.PORT || 3005;
 
   await app.listen(port);
+  // Get primsa service
+  const prismaService = app.get(PrismaService);
+
+  const imagesCount = await prismaService.imageObject.count();
 
   logger.log(`Listening on port: ${port}`);
   logger.log(`Environment: ${process.env.NODE_ENV}`);
   logger.log(`Application URL: ${await app.getUrl()}`);
+
+  logger.log(`Number of images in database: ${imagesCount}`);
 }
 bootstrap();
